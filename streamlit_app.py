4b3b185661521a5b95cbe2e08d2814c224f4b841
Bigosław Cebula <112933887+bigoslawcebula@users.noreply.github.com>
2022-09-08 08:25:10 +0200

    Update streamlit_app.py
    Added:
    my_fruit_list = my_fruit_list.set_index('Fruit')

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,11 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-# Let's put a pick list here so they can pick the fruit they want to include 
+
+#Let's set the index to be "Fruit" column header (from the CSV file), instead of the default which appears to be row number of the table
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# Let's put a pick list here so they can pick the fruit they want to include (the index value was already set to "Fruit")
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
