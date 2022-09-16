39229a57d57729f6a2ad5f6b16c97e748cb5556e
Bigosław Cebula <112933887+bigoslawcebula@users.noreply.github.com>
2022-09-16 11:34:36 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 #Let's set the index to be "Fruit" column header (from the CSV file), instead of the default which appears to be row number of the table
+#Bez tej linii, zamiast nbazw owocow wybieralibysmy ich numer
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include (the index value was already set to "Fruit")
@@ -30,7 +31,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 #streamlit.text(fruityvice_response) <--displays response code 200 (replaced with the line below)
 #streamlit.text(fruityvice_response.json) <--also incorrect, needs to be as below
 #streamlit.text(fruityvice_response.json()) <--this line will display json data, but we want it to be more readable
@@ -38,6 +39,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 # Line below takes the json version of the response and normalizes it (what ever it means)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # the line below displays the previously normalized data
+# wyglada na to, ze streamlit.dataframe poprostu wyswietli dane jakie ma (np w zmiennej) jako TABELA
 streamlit.dataframe(fruityvice_normalized)
 
 
