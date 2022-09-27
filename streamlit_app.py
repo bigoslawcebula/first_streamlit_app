cba6c5e7c7ae8ad91b0dfc505fdb8f67b06e47e5
Bigosław Cebula <112933887+bigoslawcebula@users.noreply.github.com>
2022-09-27 21:27:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,7 +82,7 @@ def get_fruityvice_data(this_fruit_choice)
     return fruityvice_normalized #nie wiem co to robi, ale to pewnie jakies zakonczenie funkcji
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?') ###pobieramy od użytkownika wartość
+  this_fruit_choice = streamlit.text_input('What fruit would you like information about?') ###pobieramy od użytkownika wartość
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:  
