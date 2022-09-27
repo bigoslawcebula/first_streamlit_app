5c5324f2ea1e977b2885f5c52903c916cfbf6496
Bigosław Cebula <112933887+bigoslawcebula@users.noreply.github.com>
2022-09-27 14:44:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,15 +23,18 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 #Bez tej linii, zamiast nbazw owocow wybieralibysmy ich numer
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# Let's put a pick list here so they can pick the fruit they want to include (the index value was already set to "Fruit")
+### Let's put a pick list here so they can pick the fruit they want to include (the index value was already set to "Fruit")
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+
+
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-
+#pobieramy dane poprzez API i zapisujemy je w zmiennej
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 #streamlit.text(fruityvice_response) <--displays response code 200 (replaced with the line below)
 #streamlit.text(fruityvice_response.json) <--also incorrect, needs to be as below
@@ -42,6 +45,13 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # the line below displays the previously normalized data
 # wyglada na to, ze streamlit.dataframe poprostu wyswietli dane jakie ma (np w zmiennej) jako TABELA
 streamlit.dataframe(fruityvice_normalized)
+
+
+###
+fruit_choice = streamlit.text_input("What fruit would you like information about?") ###pobieramy od użytkownika wartość
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice) ###wykonujemy API call z tą wartością
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) ###normalizujemy wynik (cokolwiek to oznacza)
+streamlit.dataframe(fruityvice_normalized) ###wyświetlamy wyniok w formie tabeli
 
 #######
 streamlit.stop()
