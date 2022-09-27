b2e965401b843dea91375100855deacc8eeaa518
Bigosław Cebula <112933887+bigoslawcebula@users.noreply.github.com>
2022-09-27 21:32:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,7 +86,7 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:  
-    back_from_function = get_fruityvice_data(fruit_choice)
+    back_from_function = get_fruityvice_data(this_fruit_choice)
     streamlit.dataframe(back_from_function) ###wyświetlamy wyniok w formie tabeli
 
 except URLError as e:
