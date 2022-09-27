bc21089262c3d5d49311e72bc22b71e40192dffb
Bigosław Cebula <112933887+bigoslawcebula@users.noreply.github.com>
2022-09-27 22:08:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -101,9 +101,12 @@ streamlit.header("The fruit load list contains:") #wypisujemy text na ekran
 
 ### tworzymy funkcję, którą później sobie wywołąmy, będzie się ona nazywała get_fruit_load_list
 def get_fruit_load_list(): #zdefiniowanie funkcji o nazwie get_fruit_load_list
-  with my_cnx.cursor() as my_cur ### https://linuxhint.com/cursor-execute-python/ (zdefiniowanie kursora)
+  with my_cnx.cursor() as my_cur
        my_cur.execute("select * from fruit_load_list")
        return my_cut.fetchall()
+### https://linuxhint.com/cursor-execute-python/ o kursorze i jeg funkcji w kodzie
+
+
 
 ### dodajemy przycisk (on pewnie będzie wywołuwał funkcję???)
 if streamlit.button('Get Fruit Load List'): #jeżeli ktoś naciśnie przycisk (i tylko wtedy)
